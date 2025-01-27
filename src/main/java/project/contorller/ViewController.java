4e9b490117ffc3eff71d66e27fa4691b8b3d412e
Yim_JunSu <yimjunsu@gmail.com>
2025-01-27 15:57:15 +0900

    .

--- src/main/java/project/contorller/ViewController.java
+++ src/main/java/project/contorller/ViewController.java
@@ -1,9 +1,11 @@
 package project.contorller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
+    @GetMapping("")
     public String index(){return "index.html";}
 }
